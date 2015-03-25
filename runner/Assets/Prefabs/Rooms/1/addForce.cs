961907a336e5b754968baf2a88d965c7c34242d1
bloodbrand <moguay@gmail.com>
2015-03-25 20:02:52 +0200

    scene 2
    cubes and what not

---
+++ runner/Assets/Prefabs/Rooms/1/addForce.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addForce : MonoBehaviour {
+    Ray ray;
+    RaycastHit hit;
+    [SerializeField] Transform explosion;
+	
+    void Update()
+    {
+        GetClick();
+	}
+
+    void GetClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                AddExplosion(hit.point);
+            }
+        }	
+    }
+
+    void AddExplosion(Vector3 position)
+    {
+        Instantiate(explosion, position, Quaternion.identity);
+        Collider[] colliders = Physics.OverlapSphere(position, 100);
+        foreach (Collider hit in colliders) {
+            if (hit && hit.GetComponent<Rigidbody>())
+                hit.GetComponent<Rigidbody>().AddExplosionForce(2000, position, 25, 3.0F);
+            
+        }    
+    }
+}
