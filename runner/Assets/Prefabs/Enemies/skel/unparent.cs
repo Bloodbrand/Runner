73ccb98bf8478f7c7ba82c70c66c49020a8e2f45
bloodbrand <moguay@gmail.com>
2015-04-13 22:56:56 +0300

    1: dungeon spawner

---
+++ runner/Assets/Prefabs/Enemies/skel/unparent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unparent : MonoBehaviour
+{
+    public void unparentMesh()
+    {
+        foreach (Transform child in transform)
+        {
+            Rigidbody rb = child.gameObject.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "bone")
+            unparentMesh();
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        unparentMesh();
+    }
+}
---
+++ runner/Assets/Prefabs/Spawner/dungeon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dungeon : MonoBehaviour {
+    public spawner roomSpawner;
+
+    public void SpawnRoom()
+    {
+        destroyRoom();
+        roomSpawner.spawn();
+    }
+
+    void destroyRoom()
+    {
+        int length = roomSpawner.spawnedObjects.Count;
+        if (length == 0) return;
+        GameObject oldRoom = roomSpawner.spawnedObjects[length-1].gameObject;
+        Destroy(oldRoom);
+    }
+
+
+}
